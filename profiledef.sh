59c0fbee64d756f8b8e0f691527ca9e5e8a763f3
Michaël Biewer <stuartos228@gmail.com>
2025-05-10 18:51:20 +0200

    revert

--- profiledef.sh
+++ profiledef.sh
@@ -15,7 +15,6 @@ arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
-airootfs_overlay_size="1G"
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
