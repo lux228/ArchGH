d760800ebbb563c3cfade465a45ff83fd144ce14
Michaël Biewer <stuartos228@gmail.com>
2025-05-10 15:35:19 +0200

    Test cursor.sh, step 2

---
+++ airootfs/etc/skel/.config/fish/config.fish
@@ -0,0 +1,10 @@
+# Enable starship prompt
+starship init fish | source
+
+# Set default editor
+set -x EDITOR vim
+
+# Add some useful aliases
+alias ls='ls --color=auto'
+alias ll='ls -lah'
+alias grep='grep --color=auto' 
---
+++ airootfs/root/customize_airootfs.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Set fish as default shell for the live user
+chsh -s /usr/bin/fish archiso
+
+# Enable SDDM
+systemctl enable sddm.service
+
+# Enable NetworkManager
+systemctl enable NetworkManager.service
+
+# Set up starship for root
+mkdir -p /root/.config
+cp /etc/skel/.config/starship.toml /root/.config/
+cp /etc/skel/.config/fish/config.fish /root/.config/fish/
+
+# Set fish as default shell for root
+chsh -s /usr/bin/fish root
+
+# Create a welcome message
+cat > /etc/motd << EOF
+Welcome to Arch Greybeards Hall!
+A minimal KDE-based Arch Linux distribution.
+EOF
+
+# Create build user and install yay
+useradd -m -G wheel -s /bin/bash builder
+echo "builder ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/builder
+cd /tmp
+git clone https://aur.archlinux.org/yay.git
+cd yay
+chown -R builder:builder .
+sudo -u builder makepkg -si --noconfirm
+cd /
+rm -rf /tmp/yay
+userdel -r builder
+rm /etc/sudoers.d/builder 
